dabc32d381649bb6cf850fc08c39fbf3d5ffdf96
linkesh <vslinkeshwar@gmail.com>
2026-06-25 08:57:13 +0530

    Fix: YT Transcript API

--- transcript.py
+++ transcript.py
@@ -5,23 +5,24 @@ from youtube_transcript_api import (
 
 from utils import extract_video_id
 
+
 def load_transcript(video_url: str) -> str:
 
     video_id = extract_video_id(video_url)
+
     try:
-        transcript_list = YouTubeTranscriptApi.get_transcript(
-            video_id,
-            languages=["en"],
+        api = YouTubeTranscriptApi()
+
+        transcript = api.fetch(video_id)
+
+        transcript_text = " ".join(
+            snippet.text for snippet in transcript
         )
 
-        transcript = " ".join(
-            chunk["text"] for chunk in transcript_list
-        )
-
-        return transcript
+        return transcript_text
 
     except TranscriptsDisabled:
         raise Exception("No captions available for this video.")
 
     except Exception as e:
-        raise Exception(f"Transcript Error : {e}")
+        raise Exception(f"Transcript Error: {e}")
